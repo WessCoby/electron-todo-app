fa95583c50854fa43fd9e1df712b49d26ae2bbc0
WessCoby <cobygiven@gmail.com>
2020-10-21 12:31:51 +0000

    App component setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import { ReactComponent as Hamburger } from './svg/hamburger.svg';
+import { ReactComponent as DotsVertical } from './svg/dots-vertical.svg';
+import { Sidebar, Main } from './components';
+import { useSidebarContext } from './context/sidebar.context';
+
 
 function App() {
+  const { isOpen, setIsOpen } = useSidebarContext();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="flex h-screen antialiased text-gray-900 bg-white">
+      {/* Close sidebar on outside click */}
+      <div
+        className={
+          `${isOpen ? 'block' : 'hidden'} fixed z-20 inset-0 bg-black opacity-50 transition-opacity md:hidden h-screen`
+        }
+        onClick={() => setIsOpen(false)}
+      ></div>
+      <Sidebar />
+      <div className="flex flex-col flex-1 overflow-hidden">
+        <header
+          className="flex items-center justify-between px-6 py-4 bg-gradient-to-l from-primary-500"
         >
-          Learn React
-        </a>
-      </header>
+          <div className="flex items-center">
+            <button
+              className="text-gray-500 focus:outline-none md:hidden"
+              onClick={() => setIsOpen(true)}
+            >
+              <Hamburger className="w-6 h-6 fill-current text-gray-800" />
+            </button>
+          </div>
+          <div className="flex items-center">
+          <DotsVertical className="w-6 h-6 fill-current text-gray-800" />
+          </div>
+        </header>
+        <Main />
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
