281ae40f774cefdb7b716a46d66af46814d67e61
WessCoby <cobygiven@gmail.com>
2020-10-21 12:26:30 +0000

    Added Component: TaskItem

---
+++ src/components/main/task-item.tsx
@@ -0,0 +1,67 @@
+import React, { FC, useState } from 'react';
+
+import { Task } from '../../types';
+import {
+  toggleCompleted, useAppDispatch, removeTask
+} from '../../store';
+import { UpdateTaskForm } from '..';
+import { ReactComponent as Edit } from '../../svg/edit.svg';
+import { ReactComponent as Delete } from '../../svg/delete.svg';
+
+
+type TaskItemProps = Pick<Task, 'id' | 'title' | 'icon' | 'completed'>;
+
+const TaskItem: FC<TaskItemProps> = ({ id, title, completed, icon }) => {
+  const dispatch = useAppDispatch();
+  const [isEditMode, setEditMode] = useState(false);
+
+  return !isEditMode ? (
+    <div className="my-2 flex justify-between items-center transform motion-reduce:transform-none hover:-translate-x-1 hover:-translate-y-1 hover:scale-100 transition ease-in-out duration-300">
+      <label
+        className={
+          `cursor-pointer flex items-center text-xl md:text-2xl`
+        }
+      >
+        <input
+          type="checkbox"
+          name="task"
+          id={id}
+          checked={completed}
+          onChange={
+            () => dispatch(toggleCompleted(id))
+          }
+          className="focus:outline-none focus:shadow-outline  checked:border-transparent form-checkbox text-gray-400"
+        />
+        <span className={
+            `${completed ? 'line-through text-gray-400' : 'text-gray-800'} ml-2`
+          }
+        >
+          {title}
+        </span>
+      </label>
+      <div className="inline-flex">
+        <button
+          className="hover:bg-secondary-500 hover:text-white text-secondary-500 font-bold py-2 px-4 rounded-l focus:outline-none focus:shadow-outline"
+          onClick={() => setEditMode(true)}
+        >
+          <Edit className="w-6 h-6" />
+        </button>
+        <button
+          className="hover:bg-red-500 hover:text-white font-bold py-2 px-4 rounded-r text-red-500 focus:outline-none focus:shadow-outline"
+          onClick={() => dispatch(removeTask(id))}
+        >
+          <Delete className="w-6 h-6" />
+        </button>
+      </div>
+    </div>
+  ) : (
+    <UpdateTaskForm
+      onFocus={isEditMode}
+      setter={setEditMode}
+      id={id}
+      title={title}
+    />
+  )
+}
+
+export default TaskItem;
